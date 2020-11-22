13bd9af05fc024f7c8e2838b80e7cb30f5a5057b
Justin Pricer <justin.pricer@gmail.com>
2020-11-21 19:58:04 -0600

    fix save

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# planner
-Daily Planner
+# Day Planner
+
+## Description
+
+A simple calendar app for scheduling important events during your busy day!
+
+## Instructions
+```
+Input your events in the provided text boxes. Click "Save" for each event modified. 
+
+Events are color coded based on current time:
+
+Gray: Past events
+Red: Current events
+Green: Future events
+
+Planner updates every hour on the hour.
+```
+
+## Screenshots
+
+![work day scheduler-1](./Assets/screenshot-1.png)
+![work day scheduler-2](./Assets/screenshot-2.png)
+
+## Review
+
+* Deployed Application - 05-day-planner - https://smrodriguez88.github.io/05-day-planner/
+
+* Github Repository - 05-day-planner - https://github.com/smrodriguez88/05-day-planner
--- script.js
+++ script.js
@@ -1,60 +1,154 @@
-var date = moment().format("MMM Do YY");
-// var currentTime = moment().format('H');
-// console.log(currentTime);
-
-//-------append date----------//
-$("#currentDay").append(date);
+//$(".currentDay")
+let mfull = moment().format('dddd MMMM Do YYYY, h:mm:ssa');
+let htime = moment().format('ha');
+let mdate = moment().format('MMMM-Do-YYYY');
 
 
-//---------background Updater------------------------//
-
-var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-updatetime();
-
-function updatetime() {
-  var currentTime = moment().format('H');
-  for(var i = 0; i < timeOfday.length; i++) {
-
-    if (parseInt(timeOfday[i]) > currentTime) {
-      $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");
-
-
-    }
-    else if (parseInt(timeOfday[i]) < currentTime) {
-      $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
-
-    }
-    else if (parseInt(timeOfday[i]) == currentTime) {
-      $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
-    
-    }
-  }
+let timeblocks = {
+    "12am": "",
+    "1am": "",
+    "2am": "",
+    "3am": "",
+    "4am": "",
+    "5am": "",
+    "6am": "",
+    "7am": "",
+    "8am": "",
+    "9am": "",
+    "10am": "",
+    "11am": "",
+    "12pm": "",
+    "1pm": "",
+    "2pm": "",
+    "3pm": "",
+    "4pm": "",
+    "5pm": "",
+    "6pm": "",
+    "7pm": "",
+    "8pm": "",
+    "9pm": "",
+    "10pm": "",
+    "11pm": "",
 }
 
-//--onclick event to save user input to local storage---//
-$(".rowBtn").on("click", function() {
-    var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
-});
+// Method to update the date / time
+function timeUpate(){
+    // display current day / time and styles before time interval starts
+    $("#currentDay").text(mfull)
+    styleMe()
+    // begin time interval to update time by the second, style on each update
+    interval = setInterval(function(){
+        mfull = moment().format('dddd MMMM Do YYYY, h:mm:ssa');
+        $("#currentDay").text(mfull)
+        styleMe()
+}, 1000)
+}
 
-//-------Getting individual childern and setting to parent-----//
-  $("#9am").children("input").val(localStorage.getItem("9am"));
+// For each key and value in timeblocks create a new row 
+// with 3 columns to display the time, data, and save button.
+for (let [key, value] of Object.entries(timeblocks)) {
+    // Create a new div column to display the hour
+    time = ("<div class='col-md-1 hour p-0'>"+key+"</div>")
+    // Create a new div column to display textarea for the planner data with an ID that matches the key in time blocks
+    planner_data = ("<div class='col-md-10 p-0'><textarea class='description' type='text' id='"+key+"'></textarea></div>")
+    // Create a new div column to display the saveBtn button with a time-attr that matches the key in time blocks
+    button = ("<div class='col-md-1 p-0'><button class='saveBtn' time-attr='"+key+"'>Save</button></div>")
+    // Append the row and 3 columns to the conainter class
+    $(".container").append("<div class='row'>"+time+planner_data+button+ "</div>")
+}
 
-  $("#10am").children("input").val(localStorage.getItem("10am"));
+$(".saveBtn").on("click", function(){
+    // Item equalts the data attribute "time-attr" to read
+    // the hour a particular button is tied to
+    item = $(this).attr("time-attr")
+    // Get vlaue of Input box item
+    pdata = $("#"+item).val()
+    // dynmaically set data to each Obj key
+    timeblocks[item] = pdata
+    // Save data function which stores the timeblocks Obj per day
+    save_data()
+})
 
-  $("#11am").children("input").val(localStorage.getItem("11am"));
 
-  $("#12pm").children("input").val(localStorage.getItem("12pm"));
+// Method to load data from local storage if it exists
+// if not then timeblocks object gets emptied
+function load_data(){
+    // Grab object name for the current day
+    today = localStorage.getItem(mdate)
+    // If data exists in local storage parse it
+    if(today){
+        timeblocks = JSON.parse(today)
+        // For each key and value in array set the value of
+        // each corresponding input field
+        for (let [key, value] of Object.entries(timeblocks)) {
+            $("#"+key).val(value)
+        }
+    // Else timeblocks is empty
+    } else{
+        timeblocks = {
+            "12am": "",
+            "1am": "",
+            "2am": "",
+            "3am": "",
+            "4am": "",
+            "5am": "",
+            "6am": "",
+            "7am": "",
+            "8am": "",
+            "9am": "",
+            "10am": "",
+            "11am": "",
+            "12pm": "",
+            "1pm": "",
+            "2pm": "",
+            "3pm": "",
+            "4pm": "",
+            "5pm": "",
+            "6pm": "",
+            "7pm": "",
+            "8pm": "",
+            "9pm": "",
+            "10pm": "",
+            "11pm": "",
+        }
+    }
+}
 
-  $("#1pm").children("input").val(localStorage.getItem("1pm"));
+// Method to save data to local stroage using the current date as the key
+function save_data(){
+    // Was planning to use this to add multi-day functionality
+    // but decided to pick my battles and not continue to spend
+    // more time on this assignment as I'm sure there will be
+    // future assignments that can benefit from the additional
+    // time, hehe. :)
+    localStorage.setItem(mdate, JSON.stringify(timeblocks))
+}
 
-  $("#2pm").children("input").val(localStorage.getItem("2pm"));
+// Method to style the day planner based on time
+function styleMe(){
+    // Convert current hour to military time string
+    htime = moment().format('HH')
+    for (let [key, value] of Object.entries(timeblocks)) {
+        //Convert Obj key value to military time string
+        hkey = moment(key, ["ha"]).format("HH")
+        // Conditionals to determine styling
+        if (htime == hkey){
+            $("#"+key).removeClass("past")
+            $("#"+key).removeClass("future")
+            $("#"+key).addClass("present")
+        } else if (htime > hkey){
+            $("#"+key).removeClass("present")
+            $("#"+key).removeClass("future")
+            $("#"+key).addClass("past")
+        } else if (htime < hkey){
+            $("#"+key).removeClass("present")
+            $("#"+key).removeClass("past")
+            $("#"+key).addClass("future")
+        }
+    }}
 
-  $("#3pm").children("input").val(localStorage.getItem("3pm"));
+// Do all the things!!!!
 
-  $("#4pm").children("input").val(localStorage.getItem("4pm"));
-
-  $("#5pm").children("input").val(localStorage.getItem("5pm"));
+timeUpate()
+load_data()
